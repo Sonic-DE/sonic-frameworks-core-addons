c8534d33255716997000f4a19f147ba735cec7a6
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-20 16:35:49 +0200

    KPluginMetaData: Add overload to interpret char array as string
    Otherwise the bool overload could be used by accident, which can break consumers like plasma-workspace.
    
    To make life easier for consumers the overload is deprecated and when disabling
    deprecated API is is explicitly deleted.

--- src/lib/plugin/kpluginmetadata.h
+++ src/lib/plugin/kpluginmetadata.h
@@ -436,6 +436,21 @@ public:
      */
     QString value(const QString &key, const QString &defaultValue = QString()) const;
 
+#if KCOREADDONS_ENABLE_DEPRECATED_SINCE(5, 86)
+    /**
+     * Overload to make sure the bool overload is not taken by accident
+     * @overload
+     * @since 5.86
+     */
+    KCOREADDONS_DEPRECATED_VERSION(5, 86, "Construct a QString instead of using a char array, otherwise there the bool overload could be chosen by accident")
+    QString value(const QString &key, const char *ch) const
+    {
+        return value(key, QString::fromLatin1(ch));
+    }
+#else
+    QString value(const QString &key, const char *ch) const = delete;
+#endif
+
     /**
      * @overload
      * @since 5.86
