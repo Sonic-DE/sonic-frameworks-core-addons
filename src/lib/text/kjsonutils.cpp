9592ffd2e5cda2aac72de7998da11d198f87d791
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-10-15 07:08:55 +0200

    Move utility method to read translated json values to dedicated header
    These methods are not directly related to KPluginMetaData and can
    be useful outside of it.
    
    This way KPluginMetaData only contains the logic to read properties and
    to list plugins from a namespace.
    
    Task: https://phabricator.kde.org/T14787

---
+++ src/lib/text/kjsonutils.cpp
@@ -0,0 +1,28 @@
+#include "kjsonutils.h"
+
+#include <QJsonObject>
+
+QJsonValue KJSONUtils::readTranslatedValue(const QJsonObject &jo, const QString &key, const QJsonValue &defaultValue)
+{
+    QString languageWithCountry = QLocale().name();
+    auto it = jo.constFind(key + QLatin1Char('[') + languageWithCountry + QLatin1Char(']'));
+    if (it != jo.constEnd()) {
+        return it.value();
+    }
+    const QStringView language = QStringView(languageWithCountry).mid(0, languageWithCountry.indexOf(QLatin1Char('_')));
+    it = jo.constFind(key + QLatin1Char('[') + language + QLatin1Char(']'));
+    if (it != jo.constEnd()) {
+        return it.value();
+    }
+    // no translated value found -> check key
+    it = jo.constFind(key);
+    if (it != jo.constEnd()) {
+        return jo.value(key);
+    }
+    return defaultValue;
+}
+
+QString KJSONUtils::readTranslatedString(const QJsonObject &jo, const QString &key, const QString &defaultValue)
+{
+    return KJSONUtils::readTranslatedValue(jo, key, defaultValue).toString(defaultValue);
+}
