d7cf5352ab47f243b45a63505fd6677badb6f511
David Faure <faure@kde.org>
2022-01-30 12:19:52 +0100

    KPluginMetaData: find plugins in the directory of the executable, too
    While making changes to kio_file and running KIO's jobtest, all from the
    builddir, I realized that my changes to kio_file had no impact, because
    it was still picking up the installed one. The whole point of the layout
    of the builddir (with plugins next to the executables) is for them to
    be found (and given priority).

--- src/lib/plugin/kpluginmetadata.cpp
+++ src/lib/plugin/kpluginmetadata.cpp
@@ -47,6 +47,9 @@ public:
             dirsToCheck << directory;
         } else {
             dirsToCheck = QCoreApplication::libraryPaths();
+            const QString appDirPath = QCoreApplication::applicationDirPath();
+            dirsToCheck.removeAll(appDirPath);
+            dirsToCheck.prepend(appDirPath);
 
             for (QString &libDir : dirsToCheck) {
                 libDir += QLatin1Char('/') + directory;
