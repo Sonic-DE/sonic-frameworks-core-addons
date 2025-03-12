1a19d0b88207300ad49742b118d0ce3392d55e7a
Akseli Lahtinen <akselmo@akselmo.dev>
2025-03-12 12:02:44 +0200

    Add autotest

--- src/lib/util/kshell.cpp
+++ src/lib/util/kshell.cpp
@@ -43,7 +43,7 @@ QString KShell::tildeExpand(const QString &fname)
     if (!fname.isEmpty() && fname[0] == QLatin1Char('~')) {
         int pos = fname.indexOf(QLatin1Char('/'));
         if (pos < 0) {
-            auto expandedValue = homeDir(fname.mid(1));
+            const auto expandedValue = homeDir(fname.mid(1));
             if (expandedValue.isEmpty()) {
                 return fname;
             }
