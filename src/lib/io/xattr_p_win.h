e5d7b4f7bc79e2ba724af2b32b4fe7c0c082d3ab
Méven Car <meven@kde.org>
2025-05-24 12:48:46 +0200

    unique_ptr win

--- src/lib/io/xattr_p_win.h
+++ src/lib/io/xattr_p_win.h
@@ -17,19 +17,22 @@
 
 namespace
 {
+
+using unique_file_t = std::unique_ptr<HANDLE, decltype(&CloseHandle)>;
+
 inline ssize_t k_getxattr(const QString &path, QStringView name, QString *value)
 {
-    const QString fullADSName = path + QLatin1String(":user.") + name;
-    using unique_file_t = std::unique_ptr<HANDLE, decltype(&CloseHandle)>;
+    const QString fullADSName = path + QLatin1String(":") + name;
     unique_file_t hFile(::CreateFileW(reinterpret_cast<const WCHAR *>(fullADSName.utf16()),
                                       GENERIC_READ,
                                       FILE_SHARE_READ,
                                       NULL,
                                       OPEN_EXISTING,
                                       FILE_FLAG_SEQUENTIAL_SCAN,
-                                      NULL));
+                                      NULL),
+                        &CloseHandle);
 
-    if (hFile == INVALID_HANDLE_VALUE) {
+    if (*hFile == INVALID_HANDLE_VALUE) {
         DWORD error = ::GetLastError();
         std::string message = std::system_category().message(error);
         qCWarning(KCOREADDONS_DEBUG) << "failed to open ADS:" << message << fullADSName;
@@ -37,7 +40,7 @@ inline ssize_t k_getxattr(const QString &path, QStringView name, QString *value)
     }
 
     LARGE_INTEGER lsize;
-    BOOL ret = GetFileSizeEx(hFile, &lsize);
+    BOOL ret = GetFileSizeEx(hFile.get(), &lsize);
 
     if (!ret || lsize.QuadPart > 0x7fffffff || lsize.QuadPart == 0) {
         value->clear();
@@ -47,7 +50,7 @@ inline ssize_t k_getxattr(const QString &path, QStringView name, QString *value)
     DWORD r = 0;
     QByteArray data(lsize.QuadPart, Qt::Uninitialized);
     // should we care about attributes longer than 2GiB? - unix xattr are restricted to much lower values
-    ret = ::ReadFile(hFile, data.data(), data.size(), &r, NULL);
+    ret = ::ReadFile(hFile.get(), data.data(), data.size(), &r, NULL);
 
     if (!ret) {
         DWORD error = ::GetLastError();
@@ -69,22 +72,24 @@ inline ssize_t k_getxattr(const QString &path, QStringView name, QString *value)
 
 inline int k_setxattr(const QString &path, const QString &name, const QString &value)
 {
-    const QString fullADSName = path + QLatin1String(":user.") + name;
+    const QString fullADSName = path + QLatin1String(":") + name;
     if (fullADSName.size() > MAX_PATH) {
         // https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#maximum-path-length-limitation
         // We could handle longer file names but it would require special casing per-windows version
         return ERROR_FILENAME_EXCED_RANGE;
     }
 
-    HANDLE hFile = ::CreateFileW(reinterpret_cast<const WCHAR *>(fullADSName.utf16()),
-                                 GENERIC_WRITE,
-                                 0,
-                                 NULL,
-                                 CREATE_ALWAYS,
-                                 FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,
-                                 NULL);
+    unique_file_t hFile(::CreateFileW(reinterpret_cast<const WCHAR *>(fullADSName.utf16()),
+                                      GENERIC_WRITE,
+                                      0,
+                                      NULL,
+                                      CREATE_ALWAYS,
+                                      FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,
+                                      NULL,
+                                      &CloseHandle),
+                        &CloseHandle);
 
-    if (hFile == INVALID_HANDLE_VALUE) {
+    if (hFile.get() == INVALID_HANDLE_VALUE) {
         DWORD error = ::GetLastError();
         std::string message = std::system_category().message(error);
         qCWarning(KCOREADDONS_DEBUG) << "failed to open file to write to ADS:" << message << error << fullADSName;
@@ -94,42 +99,40 @@ inline int k_setxattr(const QString &path, const QString &name, const QString &v
     DWORD count = 0;
 
     const QByteArray v = value.toUtf8();
-    if (!::WriteFile(hFile, v.constData(), v.size(), &count, NULL)) {
+    if (!::WriteFile(hFile.get(), v.constData(), v.size(), &count, NULL)) {
         DWORD error = ::GetLastError();
         std::string message = std::system_category().message(error);
         qCWarning(KCOREADDONS_DEBUG) << "failed to write to ADS:" << message << error << fullADSName;
 
-        CloseHandle(hFile);
         return -1; // unknown error
     }
 
-    CloseHandle(hFile);
     return 0; // Success
 }
 
 inline bool k_hasAttribute(const QString &path, QStringView name)
 {
-    QString fullpath = path + QLatin1String(":user.") + name;
-    HANDLE hFile = CreateFileW(reinterpret_cast<const WCHAR *>(fullpath.utf16()),
-                               GENERIC_READ,
-                               FILE_SHARE_READ | FILE_SHARE_WRITE,
-                               NULL,
-                               OPEN_EXISTING,
-                               FILE_ATTRIBUTE_NORMAL,
-                               NULL);
+    QString fullpath = path + QLatin1String(":") + name;
+    unique_file_t hFile(::CreateFileW(reinterpret_cast<const WCHAR *>(fullpath.utf16()),
+                                      GENERIC_READ,
+                                      FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                      NULL,
+                                      OPEN_EXISTING,
+                                      FILE_ATTRIBUTE_NORMAL,
+                                      NULL),
+                        &CloseHandle);
 
-    if (hFile == INVALID_HANDLE_VALUE) {
+    if (hFile.get() == INVALID_HANDLE_VALUE) {
         // ERROR_FILE_NOT_FOUND supposedly
         return false;
     }
     // ADS exists
-    CloseHandle(hFile);
     return true;
 }
 
 inline int k_removexattr(const QString &path, QStringView name)
 {
-    const QString fullADSName = path + QLatin1String(":user.") + name;
+    const QString fullADSName = path + QLatin1String(":") + name;
     int ret = (DeleteFileW(reinterpret_cast<const WCHAR *>(fullADSName.utf16()))) ? 0 : -1;
     return ret;
 }
@@ -151,15 +154,16 @@ QStringList k_queryAttributes(QStringView path)
         return fileAttributes;
     }
 
-    HANDLE hFile = ::CreateFile(reinterpret_cast<const WCHAR *>(path.utf16()),
-                                GENERIC_READ,
-                                FILE_SHARE_READ | FILE_SHARE_WRITE,
-                                NULL,
-                                OPEN_EXISTING,
-                                FILE_ATTRIBUTE_NORMAL,
-                                NULL);
+    unique_file_t hFile(::CreateFile(reinterpret_cast<const WCHAR *>(path.utf16()),
+                                     GENERIC_READ,
+                                     FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                     NULL,
+                                     OPEN_EXISTING,
+                                     FILE_ATTRIBUTE_NORMAL,
+                                     NULL),
+                        &CloseHandle);
 
-    if (hFile == INVALID_HANDLE_VALUE) {
+    if (hFile.get() == INVALID_HANDLE_VALUE) {
         DWORD error = ::GetLastError();
         std::string message = std::system_category().message(error);
         qCWarning(KCOREADDONS_DEBUG) << "failed CreateFile:" << message << path << error;
@@ -169,13 +173,13 @@ QStringList k_queryAttributes(QStringView path)
     QList<QByteArray> entries;
     QString entry;
     FILE_STREAM_INFO *fi = new FILE_STREAM_INFO[256];
-    if (::GetFileInformationByHandleEx(hFile, FileStreamInfo, fi, 256 * sizeof(FILE_STREAM_INFO))) {
+    if (::GetFileInformationByHandleEx(hFile.get(), FileStreamInfo, fi, 256 * sizeof(FILE_STREAM_INFO))) {
         // ignore first entry it is "::$DATA"
         FILE_STREAM_INFO *p = fi;
         while (p->NextEntryOffset != NULL) {
             p = (FILE_STREAM_INFO *)((char *)p + p->NextEntryOffset);
             entry = QString::fromUtf16((char16_t *)p->StreamName, p->StreamNameLength / sizeof(char16_t));
-            // entries are of the form ":user.key:$DATA"
+            // entries are of the form ":key:$DATA"
             entry.chop(6);
             entries.append(entry.sliced(1).toLocal8Bit());
         }
@@ -183,11 +187,10 @@ QStringList k_queryAttributes(QStringView path)
         DWORD error = ::GetLastError();
         if (error != ERROR_HANDLE_EOF) {
             std::string message = std::system_category().message(error);
-            qCWarning(KCOREADDONS_DEBUG) << "failed GetFileInformationByHandleEx:" << message << path << hFile;
+            qCWarning(KCOREADDONS_DEBUG) << "failed GetFileInformationByHandleEx:" << message << path << hFile.get();
         }
     }
     delete[] fi;
-    CloseHandle(hFile);
 
     if (entries.size() == 0) {
         return fileAttributes;
